25b76b735315e24c0035f5a31788e8329c7308d9
Sebastian Semmler <sebastian@semmler.pro>
2016-06-14 14:01:51 +0200

    symlinks files to home folder

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+ln -s /Users/basti/dotfiles/.zsh /Users/basti
+ln -s /Users/basti/dotfiles/.zshrc /Users/basti
+ln -s /Users/basti/dotfiles/.vim /Users/basti
+ln -s /Users/basti/dotfiles/.vimrc /Users/basti
+ln -s /Users/basti/dotfiles/.tmux.conf /Users/basti
+#git submodule init && git submodule update
