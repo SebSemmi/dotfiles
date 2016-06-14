d95348c8ae7c6ee6a752f5a3e455f45647eadb4d
Sebastian Semmler <sebastian@semmler.pro>
2016-06-14 15:32:49 +0200

    symlinks added

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,6 @@ ln -s /Users/basti/dotfiles/.zshrc /Users/basti
 ln -s /Users/basti/dotfiles/.vim /Users/basti
 ln -s /Users/basti/dotfiles/.vimrc /Users/basti
 ln -s /Users/basti/dotfiles/.tmux.conf /Users/basti
+ln -s /Users/basti/dotfiles/.zlogin /Users/basti
+ln -s /Users/basti/dotfiles/.gvimrc /Users/basti
 #git submodule init && git submodule update
