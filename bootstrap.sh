a4c1b0926971f3790857ba1356660bc665fffced
Sebastian Semmler <sebastian@semmler.pro>
2016-08-17 16:47:21 +0200

    symlink option set

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-ln -s ~/dotfiles/.zsh ~
-ln -s ~/dotfiles/.zshrc ~
-ln -s ~/dotfiles/.vim ~
-ln -s ~/dotfiles/.vimrc ~
-ln -s ~/dotfiles/.tmux.conf ~
-ln -s ~/dotfiles/.zlogin ~
-ln -s ~/dotfiles/.gvimrc ~
-ln -s ~/dotfiles/.gitignore_global ~
+ln -sF ~/dotfiles/.zsh ~
+ln -sF ~/dotfiles/.zshrc ~
+ln -sF ~/dotfiles/.vim ~
+ln -sF ~/dotfiles/.vimrc ~
+ln -sF ~/dotfiles/.tmux.conf ~
+ln -sF ~/dotfiles/.zlogin ~
+ln -sF ~/dotfiles/.gvimrc ~
+ln -sF ~/dotfiles/.gitignore_global ~
 mkdir ~/.vim/backups
 git submodule init && git submodule update
 git config --global core.excludesfile ~/.gitignore_global
