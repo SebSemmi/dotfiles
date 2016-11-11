086905de91b37d9d766c9dd9f447634e5f13ac1d
Sebastian Semmler <sebastian@semmler.pro>
2016-11-12 00:02:49 +0100

    Typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-ln -sF ~/dotfiles/.zsh ~
-ln -sF ~/dotfiles/.zshrc ~
-ln -sF ~/dotfiles/.vim ~
-ln -sF ~/dotfiles/.vimrc ~
-ln -sF ~/dotfiles/.tmux.conf ~
-ln -sF ~/dotfiles/.zlogin ~
-ln -sF ~/dotfiles/.gvimrc ~
-ln -sF ~/dotfiles/.gitignore_global ~
+
+DOTFILES_DIR="~/dotfiles"
+
+ln -sF $DOTFILES_DIR/.zsh ~
+ln -sF $DOTFILES_DIR/.zshrc ~
+ln -sF $DOTFILES_DIR/.vim ~
+ln -sF $DOTFILES_DIR/.vimrc ~
+ln -sF $DOTFILES_DIR/.tmux.conf ~
+ln -sF $DOTFILES_DIR/.zlogin ~
+ln -sF $DOTFILES_DIR/.gvimrc ~
+ln -sF $DOTFILES_DIR/.gitignore_global ~
 mkdir ~/.vim/backups
-git submodule init && git submodule update
+git submodule update --init
 git config --global core.excludesfile ~/.gitignore_global
