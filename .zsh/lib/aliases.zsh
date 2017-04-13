7837e7aded33b5ca1f420c59873fb8f41c838301
Sebastian Semmler <sebastian@semmler.pro>
2017-04-13 20:30:37 +0200

    Set tmux alias to always us UTF-8

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -3,3 +3,5 @@ alias lh='ls -lah'
 alias ...='cd ../..'
 alias launchpadreset="defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock"
 alias osxinstaller="sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Install\ macOS\ Sierra --applicationpath /Applications/Install\ macOS\ Sierra.app"
+alias now='date +%s'
+alias tmux='tmux -u'
