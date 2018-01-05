fa52b2ce3e4763263085c0fa1931684baeb89a3d
Sebastian Semmler <sebastian@semmler.pro>
2018-01-05 16:21:54 +0100

    fix zsh aliases

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -2,6 +2,6 @@ alias ll='ls -lahL'
 alias lh='ls -lah'
 alias ...='cd ../..'
 alias launchpadreset="defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock"
-alias osxinstaller="sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Install\ macOS\ Sierra --applicationpath /Applications/Install\ macOS\ Sierra.app"
+alias osxinstaller="sudo /Applications/Install\ macOS\ High\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Install\ macOS\ High\ Sierra --applicationpath /Applications/Install\ macOS\ Sierra.app"
 alias now='date +%s'
 alias tmux='tmux -u'
